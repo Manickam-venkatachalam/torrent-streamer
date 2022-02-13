7da5956ce3335d49685d4c4215e53364fabba8ce
Manickam <72659782+Manickam-venkatachalam@users.noreply.github.com>
2022-02-13 22:30:21 +0530

    first commit

---
+++ src/components/Video.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Player from "react-player";
+
+
+const Video = ({
+  title,
+  description = "My Video Description",
+  url = "https://torrentdownload-1.s3.ap-south-1.amazonaws.com/Mahaan.mkv",
+}) => {
+  return (
+    <>
+      <div className=" my-3 ">
+        <div className="">
+          <Player width={"95%"} height={"100%"} controls url={url} />
+        </div>
+        <div className="my-4 md:text-xl">
+          <h3 className="font-medium">{title}</h3>
+          <h3 className="my-2 md:text-sm text-gray-500">{description}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Video;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Video from "../components/Video";
+import YoutubeLogo from "../assets/youtube-logo.png";
+import Upcoming from "../components/Upcoming";
+const Home = () => {
+  return (
+    <div className="my-2 ml-5">
+      <a href="#" className="flex items-center space-x-1">
+        <img src={YoutubeLogo} className="w-10 h-10" alt="Logo" />
+        <h2 className="font-bold">MyTube</h2>
+      </a>
+      <div className="md:flex">
+        <div className=" md:w-9/12">
+          <Video
+            title="Bachelor Movie | Youtube"
+            description="Bachelor movie GV Prakash and Divya bharathi"
+            url={
+              "https://sgp1.digitaloceanspaces.com/torrentdownload/example%0A/Veerama_Vaagai_Soodum.mkv"
+            }
+          />
+        </div>
+
+        <div className="md:w-3/12">
+          <Upcoming />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
