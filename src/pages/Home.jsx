73e3161cf5b733bfbfd1b5417358b48b2698a813
Manickam <72659782+Manickam-venkatachalam@users.noreply.github.com>
2022-02-14 12:40:56 +0530

    test_update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,7 @@ const Home = () => {
             title="Bachelor Movie | Youtube"
             description="Bachelor movie GV Prakash and Divya bharathi"
             url={
-              "https://nyc3.digitaloceanspaces.com/streamer/example/Akash_Vaani_EP01%EA%9E%89Best_Laid_Plans.mkv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AMDAOAOHZLMUO4LTFAML%2F20220214%2Fnyc3%2Fs3%2Faws4_request&X-Amz-Date=20220214T065410Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=e134a114ed8c792449f5a89b6cf18d8fea4634e830f23a1378f1f9b7fc2bbdb1"
-            }
+              "https://torrentdownload-1.s3.ap-south-1.amazonaws.com/Akash_Vaani_EP01%EA%9E%89Best_Laid_Plans.mkv"
           />
         </div>
 
